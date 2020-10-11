0dfea710e3d2df7f4aac3c791e7f1bbbe9a53219
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-10-11 18:08:27 -0500

    levantando servidor

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,21 @@
-console.log("PROBANDO");
+import express from "express";
+import cors from "cors";
+import compression from "compression";
+import { createServer } from "http";
+
+const app = express();
+app.use("*", cors());
+app.use(compression());
+
+app.get("/", (req, res) => {
+  res.send("PROBANDO");
+});
+
+const httpServer = createServer(app);
+
+httpServer.listen(
+  {
+    port: 3000,
+  },
+  () => console.log("Servidor levantado en http://localhost:3000")
+);
