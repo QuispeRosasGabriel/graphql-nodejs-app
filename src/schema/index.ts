b7f9633f676913551932fb697ce8b9f2f4350420
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-10-11 23:24:05 -0500

    ejecutando schema

---
+++ src/schema/index.ts
@@ -0,0 +1,10 @@
+import 'graphql-import-node';
+import typeDefs from './schema.graphql';
+import { makeExecutableSchema } from 'graphql-tools';
+import { GraphQLSchema } from 'graphql';
+
+const schema: GraphQLSchema = makeExecutableSchema({
+    typeDefs
+});
+
+export default schema;
