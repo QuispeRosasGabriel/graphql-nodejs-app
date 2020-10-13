2028211a3accf3763c29074857ecf7a6712055d7
Gabriel Quispe Rosas <gabrielquisperosas@MacBook-Pro-de-Gabriel.local>
2020-10-12 20:33:29 -0500

    seteando schema

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import compression from 'compression';
 import { createServer } from 'http';
 
 import environments from './config/environments';
+import { ApolloServer } from 'apollo-server-express';
+import schema from './schema';
+import expressPlayground from 'graphql-playground-middleware-express';
 //Configuracion de las variables de entorno (lectura)
 if (process.env.NODE_ENV !== 'production') {
   const env = environments;
@@ -15,9 +18,16 @@ const init = async () => {
   app.use('*', cors());
   app.use(compression());
 
-  app.get('/', (req, res) => {
-    res.send('PROBANDO xd');
-  });
+  const server = new ApolloServer({
+    schema,
+    introspection: true
+  })
+
+  server.applyMiddleware({app})
+
+  app.get('/', expressPlayground({
+    endpoint: '/graphql'
+  }));
 
   const httpServer = createServer(app);
   const PORT = process.env.PORT || 4000;
