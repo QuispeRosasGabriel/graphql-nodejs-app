c043af289835e93d487a78d0e082af00dbd22a90
Gabriel Quispe Rosas <gabrielquisperosas@MacBook-Pro-de-Gabriel.local>
2020-10-14 21:21:33 -0500

    schema

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import environments from './config/environments';
 import { ApolloServer } from 'apollo-server-express';
 import schema from './schema';
 import expressPlayground from 'graphql-playground-middleware-express';
+import Database from './lib/database';
+import { isContext } from 'vm';
 //Configuracion de las variables de entorno (lectura)
 if (process.env.NODE_ENV !== 'production') {
   const env = environments;
@@ -18,9 +20,14 @@ const init = async () => {
   app.use('*', cors());
   app.use(compression());
 
+  const database = new Database();
+  const db = await database.init()
+  const context = {db};
+
   const server = new ApolloServer({
     schema,
-    introspection: true
+    introspection: true,
+    context,
   })
 
   server.applyMiddleware({app})
