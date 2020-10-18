e37c26d30fca912ca0fa8536f9223d2e480aa2d5
Gabriel Quispe Rosas <gabrielquisperosas@MacBook-Pro-de-Gabriel.local>
2020-10-18 12:08:31 -0500

    schema login

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,28 +1,42 @@
 import { IResolvers } from 'graphql-tools';
-
-// id: ID!
-// name: String!
-// lastname: String!
-// emai: String!
-// password: String!
-// registerDate: String!
-// birthdate: String!
+import { COLLECTIONS } from '../config/constants';
 
 const resolversQuery: IResolvers = {
     Query: {
-        users(root, args, context, info) {
-            console.log(root, args, context, info);
-            return [
-                {
-                    id: '1',
-                    name: 'Gabriel',
-                    lastname: 'Quispe',
-                    email: 'gabo@gmail.com',
-                    passowrd: 'xddd',
-                    registerDate: '23-02-1203',
-                    birthdate: '12-12-1234'
+        async users(root, args, {db}) {
+            try {
+                return {
+                    status: true,
+                    message: "Lista de usuarios cargada correctamente",
+                    users: await db.collection(COLLECTIONS.USERS)
+                    .find().toArray()
+                }               
+            } catch (error) {
+               console.log(error);
+               return {
+                   status: false,
+                   message: error,
+                   users: []
+               }; 
+            }
+        },
+        async login(root, {email, password},{db}){
+            try {
+                const user = await db.collection(COLLECTIONS.USERS)
+                .findOne({email, password}) 
+                return{
+                    status: true,
+                    message: (user ? 'Lista de usuarios cargada corretamente' : 'Credenciales incorrectas, sesión no iniciada'),
+                    user
                 }
-            ];
+            } catch (error) {
+                return {
+                    status: false,
+                    message: error,
+                    user: null
+                }
+            }
+
         }
     }
 };
